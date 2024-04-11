dfe74fca78233dc3b468c94b1a93fb6e88cf2dcd
ahndb <asi0000@nate.com>
2024-04-11 10:50:34 +0900

    240411 10:50 Updatee inputBox

--- src/components/Inputbox/index.tsx
+++ src/components/Inputbox/index.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './style.css';
 
-interface InputBoxProps {
+export interface InputBoxProps {
   label: string;
   type: 'text' | 'password';
+  value: string;
   placeholder: string;
-  button?: string;
+  onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+  buttonTitle?: string;
 }
 
-
-export default function InputBox({label, type, placeholder, button}: InputBoxProps) {
+export default function InputBox({ label, type, value, placeholder, onChangeHandler, buttonTitle }: InputBoxProps) {
 
   return (
     <div className="input-box">
       <div className="input-label label">{label}</div>
       <div className="input-content-box">
-          <input className="input" type={type} placeholder={placeholder} />
-          <div className="input-disable-button">{button}</div>
+          <input
+              className="input"
+              type={type}
+              value={value}
+              placeholder={placeholder}
+              onChange={onChangeHandler}
+          />
+          { buttonTitle && 
+          <div className="input-disable-button">
+              {buttonTitle}
+          </div> 
+          }
       </div>
       <div className="input-message"></div>
     </div>
-  )
+  );
 }
